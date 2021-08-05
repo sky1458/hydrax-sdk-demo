b331fade85e8ae7cf2d7b3f547218d7b23b37fcb
daigang1228 <dg17729840156@163.com>
2021-08-05 13:54:50 +0800

    1.3.1

--- src/notif/notif.ts
+++ src/notif/notif.ts
@@ -1,14 +1,20 @@
 import { AxiosRequestConfig } from 'axios';
 
-// import { ErrorHandler, SDKConfig } from '../index.d';
 import request from "../utils/request";
 
+
+ /**
+  * 
+  * This function gets user notifications
+   @options type:AxiosRequestConfig,Optional parameters passed in: { params:{ limit:10, offset:10} }
+   @returns return object
+ */
 
 export async function notif( optionsDefault?: SDKConfig, options:AxiosRequestConfig={},errorHandler?: ErrorHandler) {
   const { token,user_id,url } = optionsDefault;
   const cond = [['user_id', '=', user_id]];
   const fields = ['time', 'title', 'state', 'data'];
-  const {limit=10, offset=10}  =  options.params
+  const { limit=10, offset=10 }  =  options.params
   const params = [
     'notif',
     'search_read_path',
