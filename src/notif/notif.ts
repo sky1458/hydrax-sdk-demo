e52c0b8dfba67181ca642a3f55cd1cad6b0e4928
daigang1228 <dg17729840156@163.com>
2021-08-05 15:42:14 +0800

    1.4.1

--- src/notif/notif.ts
+++ src/notif/notif.ts
@@ -1,43 +1,47 @@
 import { AxiosRequestConfig } from 'axios';
 
-import request from "../utils/request";
+import request from '../utils/request';
 
-
- /**
+/**
   * 
   * This function gets user notifications
    @options type:AxiosRequestConfig,Optional parameters passed in: { params:{ limit:10, offset:10} }
    @returns return object
  */
 
-export async function notif( optionsDefault?: SDKConfig, options:AxiosRequestConfig={},errorHandler?: ErrorHandler) {
-  const { token,user_id,url } = optionsDefault;
+export async function notif(
+  optionsDefault?: SDKConfig,
+  options: AxiosRequestConfig = {},
+  errorHandler?: ErrorHandler
+) {
+  const { token, user_id, url } = optionsDefault;
   const cond = [['user_id', '=', user_id]];
   const fields = ['time', 'title', 'state', 'data'];
-  const { limit=10, offset=10 }  =  options?.params
+  const { params: pms = {} } = options;
+  const { limit = 10, offset = 10 } = pms;
   const params = [
     'notif',
     'search_read_path',
     [cond, fields],
-    { order: 'time desc', count: true, limit, offset},
+    { order: 'time desc', count: true, limit, offset },
     {
-      "user_id": user_id,
-      "token": token,
-    }
+      user_id: user_id,
+      token: token,
+    },
   ];
-  
-  const optionsData:AxiosRequestConfig = {
-    data:{
+
+  const optionsData: AxiosRequestConfig = {
+    data: {
       id: new Date().getTime(),
       method: 'execute',
       params,
-    }
-  }
-  
+    },
+  };
+
   const response = await request(url, optionsData, errorHandler);
-  if(!response.data.error){
-    return { data:response.data.result[0], total:response.data.result[1]}  
-  }else{
-    return { error: response.data.error}
+  if (!response.data.error) {
+    return { data: response.data.result[0], total: response.data.result[1] };
+  } else {
+    return { error: response.data.error };
   }
 }
