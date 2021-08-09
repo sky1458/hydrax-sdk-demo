029276731e882c299ef6190ce2bdf701609f9378
yanghongwei <>
2021-08-09 11:15:35 +0800

    1.4.6

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-import { AxiosRequestConfig } from 'axios';
-
 import { getCryptoAddress, getBalances, getUserInfo } from './account';
-import { getAddress } from './address/getAddress';
 import { queryNotificationList } from './notification';
 
 function HydraxSDK(config: SDKConfig) {
